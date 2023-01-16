6e7e0d309d42cf19d7ce74ebcf706f029b91f2a5
Fernando Ultremare <feroult@gmail.com>
2023-01-16 13:19:29 -0300

    adding stuff

---
+++ app/getch.py
@@ -0,0 +1,21 @@
+try:
+    import msvcrt
+    getch = msvcrt.getch
+except:
+    import sys
+    import tty
+    import termios
+
+    def _unix_getch():
+        """Get a single character from stdin, Unix version"""
+
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())          # Raw read
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    getch = _unix_getch

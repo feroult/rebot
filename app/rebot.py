8d0c551e94dee32b8a309e0b297e44ff87790838
Fernando Ultremare <feroult@gmail.com>
2023-01-16 22:38:14 -0300

    adding stuff

--- app/rebot.py
+++ app/rebot.py
@@ -1,16 +1,22 @@
-from prompt_toolkit import PromptSession
-from prompt_toolkit.key_binding import KeyBindings
-
-bindings = KeyBindings()
-
-@bindings.add('escape', 'x')
-def _(event):
-    event.current_buffer.validate_and_handle()
-
-session = PromptSession()
+import chat_gpt
+import console
+import formatter
 
 
-if __name__ == '__main__':
-    while True:
-        answer = session.prompt('Give me some input: ', key_bindings=bindings, multiline=True)
-        print('You said: %s' % answer)
+history = ""
+toggle_highlight = False
+
+# Start the conversation
+while True:
+    prompt = console.prompt("# ")
+
+    if prompt == "/r":
+        print("\nchat restarted.\n")
+        history = ""
+    elif prompt == "/hl":
+        toggle_highlight = not toggle_highlight
+        print(f"\nhighlight {'on' if toggle_highlight else 'off'}.\n")
+    else:
+        response = chat_gpt.generate_response(prompt, history)
+        print(formatter.format(response, toggle_highlight))
+        history += f"{prompt}\n{response}\n"
